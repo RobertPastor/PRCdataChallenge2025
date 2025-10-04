3c9d4fe2a1bb67ce0d0a207d784dc2f1fd97bbcc
Robert <robert.pastor0691@orange.fr>
2025-10-04 23:27:33 +0200

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,34 @@
+## create repo 
+## setup current dir to the repo
+cd "C:\Users\rober\git\PRCdataChallenge2025"
+
+## create a git repo
+git init
+-> check the word (master) showing the current branch
+
+## create the virtual environment
+
+$ python -m venv PRCdataChallenge
+
+rober@RobertPastor MINGW64 ~/git/PRCdataChallenge2025 (master)
+
+## check virtual env
+$ ls -al PRCdataChallenge
+total 5
+drwxr-xr-x 1 rober 197609   0 Oct  4 22:27 ./
+drwxr-xr-x 1 rober 197609   0 Oct  4 22:27 ../
+drwxr-xr-x 1 rober 197609   0 Oct  4 22:27 Include/
+drwxr-xr-x 1 rober 197609   0 Oct  4 22:27 Lib/
+drwxr-xr-x 1 rober 197609   0 Oct  4 22:27 Scripts/
+-rw-r--r-- 1 rober 197609 341 Oct  4 22:27 pyvenv.cfg
+
+## activate the virtual environment
+$ source PRCdataChallenge/Scripts/activate
+(PRCdataChallenge)
+rober@RobertPastor MINGW64 ~/git/PRCdataChallenge2025 (master)
+$
+## check python version
+$ python --version
+Python 3.11.3
+(PRCdataChallenge)
+rober@RobertPastor MINGW64 ~/git/PRCdataChallenge2025 (master)
---
+++ trajectory/Environment/Constants.py
@@ -0,0 +1,73 @@
+'''
+Created on 17 déc. 2020
+
+@author: robert
+
+Define constants common to all modules
+
+'''
+
+gravity_meter_square_seconds = 9.80665 
+
+Meter2Feet = 3.2808399 # one meter equals 3.28 feet
+#Meter2Feet = 3.2808399 # one meter approx == 3 feet (3 feet 3⅜ inches)
+
+Feet2Meter = 0.3048 # one feet equals 0.3048 meters
+
+Meter2NauticalMiles = 0.000539956803 # One Meter = 0.0005 nautical miles
+
+NauticalMiles2Meter = 1852.0 # in meters
+NauticalMiles2Meters = 1852.0 # in meters
+
+Knots2MetersSeconds   = 0.514444444 # meters / second
+Knots2MetersPerSecond = 0.514444444 # meters / second
+Knot2MetersPerSecond  = 0.514444444 # meters per second
+
+MeterPerSecond2Knots = 1.9438444924406
+MeterSecond2Knots    = 1.9438444924406
+
+MeterSeconds2FeetMinutes = 196.85039370078738
+
+# mass
+
+Kilogram2Pounds = 2.20462262
+Kilogram2Pounds = 2.20462262 # 1 kilogram = 2.204 lbs
+
+KeroseneLiter2Kilograms = 0.817
+KeroseneKilogram2Liter = 1.2345679012345678
+
+# minimal radius of the final turn -> /flight-profile/trajectory/Guidance/TurnLegFile.py
+FinalArrivalTurnRadiusNauticalMiles = 5.0
+ThreeDegreesGlideSlope = 3.0
+
+DescentGlideSlopeThreeDegrees = 3.0
+DescentGlideSlopeDistanceNauticalMiles = 10.0
+
+MaxRateOfClimbFeetPerMinutes = 2000.0
+MaxRateOfDescentFeetPerMinutes = -2200.0
+
+Kerosene_kilo_to_US_gallons = 0.33
+US_gallon_to_US_dollars = 3.25
+
+GravityMetersPerSquareSeconds = 9.81
+
+MinFlightLevel = 15.0
+MaxFlightLevel = 450.0
+
+''' 23rd July 2023 - Reduced Climb Power - see BADA manual 3.10 '''
+reducedClimbPowerJetCoeffMaxValue = 0.15
+
+ConstantClimbRampLengthNauticalMiles = 5.0 # Nautical Miles
+GlideSlopeStart2TouchDownNauticalMiles = 5.0 # 5 Nmfrom start of glide slope to runway touch down
+
+RollingFrictionCoefficient = 0.035
+
+ConstantTaxiSpeedCasKnots = 5.0 # Knots CAS ???
+
+EarthRadiusMeters     = 6378135.0 # earth’s radius in meters
+EarthMeanRadiusMeters = 6378135.0 # earth’s radius in meters
+
+MaxSpeedNoiseRestrictionsKnots = 250.0
+MaxSpeedNoiseRestrictionMeanSeaLevelFeet = 10000.0
+
+FeetMinutes2MetersSeconds = 0.00508
