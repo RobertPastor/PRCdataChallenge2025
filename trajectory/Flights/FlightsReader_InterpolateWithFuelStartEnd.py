1dfe53f0f9d349bf076bf6fa8bc245c23fda6c43
Robert <robert.pastor0691@orange.fr>
2025-11-09 10:08:16 +0100

    create interpolated flights data files

--- trajectory/Flights/FlightsReader_InterpolateWithFuelStartEnd.py
+++ trajectory/Flights/FlightsReader_InterpolateWithFuelStartEnd.py
@@ -11,10 +11,40 @@ import logging
 import unittest
 import pandas as pd
 import os
+import numpy as np
 from trajectory.Flights.FlightsReader import FlightsDatabase
 from tabulate import tabulate
 from trajectory.Fuel.FuelReader import FuelDatabase
+from tabulate import tabulate
 
+from trajectory.utils import keepOnlyColumns
+
+def fill_Fuel_Frame_with_empty_columns_for_interpolation(df):
+    
+    listOfColumns = ['latitude', 'longitude', 'altitude', 'groundspeed', 'track', 'vertical_rate', 'mach', 'TAS', 'CAS']
+    # Add an empty column (filled with NaN)
+    for columnName in listOfColumns:
+        df[columnName] = np.nan
+    
+    return df
+
+def build_Fuel_Dataframe_from_start_end( fuelTrainDataframe ):
+    
+    fuelStartDataframe = fuelTrainDataframe.copy()
+    listOfColumnNamesToKeep = ['flight_id', 'start']
+    fuelStartDataframe = keepOnlyColumns( fuelStartDataframe, listOfColumnNamesToKeep)
+    fuelStartDataframe = fuelStartDataframe.rename(columns={'start': 'timestamp'})
+    print ( list ( fuelStartDataframe ))
+    print ( fuelStartDataframe.shape  )
+    
+    fuelEndDataframe = fuelTrainDataframe.copy()
+    listOfColumnNamesToKeep = ['flight_id', 'end']
+    fuelEndDataframe = keepOnlyColumns( fuelEndDataframe, listOfColumnNamesToKeep)
+    fuelEndDataframe = fuelEndDataframe.rename(columns={'end': 'timestamp'})
+    print ( list ( fuelEndDataframe ))
+    print ( fuelEndDataframe.shape  )
+
+    return fuelStartDataframe , fuelEndDataframe
 
 #============================================
 class Test_Main(unittest.TestCase):
@@ -23,24 +53,50 @@ class Test_Main(unittest.TestCase):
         
         train_rank = "train"
         ''' open fuel database '''
-        fuelDatabase = FuelDatabase()
-        fuelTrainDataframe = fuelDatabase.fuelDatabasereadFuelTrainLite()
+        count_of_files = 10
+        fuelDatabase = FuelDatabase(count_of_files)
+        
+        fuelTrainDataframe = fuelDatabase.readFuelTrainLite()
         print ( list ( fuelTrainDataframe ))
+        
+        fuelTrainReducedWithStartDataframe , fuelTrainReducedWithEndDataframe = build_Fuel_Dataframe_from_start_end (fuelTrainDataframe)
+        fuelTrainDataframe = pd.concat( [fuelTrainReducedWithStartDataframe , fuelTrainReducedWithEndDataframe] )
+        print ( fuelTrainDataframe.shape )
+        print ( list ( fuelTrainDataframe ) )
+        
+        print(tabulate(fuelTrainDataframe[:10], headers='keys', tablefmt='grid' , showindex=False , ))
+
         
         '''loop through the files '''
         flightsDatabase = FlightsDatabase()
         
         directory = flightsDatabase.getTrainFlightsFolderPathStr()
+        maxCount = 10
+        count = 0
         for fileName in os.listdir(directory):
-            if fileName.endswith(".parquet"): # Filter specific file types
-                filePath = os.path.join(directory, fileName)
-                print ( filePath )
-                
-                flight_id = fileName.split("\\.")[0]
-                print("flight_id = " + flight_id)
-                
-                flightTrainDataframe = flightsDatabase.readOneTrainFileLite(fileName)
-                print ( list ( flightTrainDataframe ))
+            if count < maxCount:
+                if fileName.endswith(".parquet"): # Filter specific file types
+                    filePath = os.path.join(directory, fileName)
+                    print ( filePath )
+                    
+                    flight_id = fileName.split("\\.")[0]
+                    print("flight_id = " + flight_id)
+                    
+                    flightTrainDataframe = flightsDatabase.readOneTrainFileLite(fileName)
+                    print ( flightTrainDataframe.shape )
+                    
+                    ''' filter fuel on flight id and perform concat '''
+                    ''' in order for the fuel start and end to exist as new rows in the flight dataframe '''                    
+                    filteredFuelDataframe = fuelTrainDataframe[fuelTrainDataframe['flight_id'] == flight_id]
+                    print ( filteredFuelDataframe.shape )
+
+                    ''' concat the dataframe '''
+                    flightTrainDataframe = pd.concat ( [flightTrainDataframe , filteredFuelDataframe])
+                    print ( flightTrainDataframe.shape )
+                    #print ( list ( flightTrainDataframe ) )
+                    
+                    
+            count = count + 1
         
       
 if __name__ == '__main__':
